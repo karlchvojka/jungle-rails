0632236c8a3ab9e5dac7c9ba842f3fff80bfed22
Karl Chvojka <karl.chvojka@gmail.com>
2019-05-16 20:26:52 +0000

    Updated application controller, added a session, and controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -18,10 +18,16 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
-
+  # Routs for login
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   # Routes for the User management section
   get '/register' => 'users#new'
   post '/users' => 'users#create'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
