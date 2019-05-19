03188515d84c624fc448111d0eb7adc7626e1b55
Karl <karl.chvojka@gmail.com>
2019-05-19 01:18:07 -0400

    Finished Basic HTTP auth for Admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,4 +38,9 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
+  def authenticate
+   authenticate_or_request_with_http_basic do |username, password|
+     username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+  end
+ end
 end
