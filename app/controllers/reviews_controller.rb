b2c472216b2dd5b75cffb34d5b65b804543fc868
Karl <karl.chvojka@gmail.com>
2019-05-20 15:36:57 -0400

    Review form is submitting, and variables are being stored

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @product = Product.find params[:product_id]
+    @review = @product.reviews.new review_params
+    @review.user_id = current_user
+
+    if @review.save
+      redirect_to @product, notice: "Review created"
+    else
+      redirect_to @product, notice: "Review not created error."
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
 end
