89dc4a18bf4ae1d140042174cd3a90e85649fa5b
Karl <karl.chvojka@gmail.com>
2019-05-21 16:39:17 -0400

    Added product test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @category = Category.find_or_create_by! name: 'Test'
+    @product = Product.new(name: 'Chair', description: 'A chair', price: 250, quantity: 4, category: @category)
+  end
+
   describe 'Validations' do
+    it "should be a valid product, with all valid parameters" do
+      expect(@product).to be_valid
+    end
+
+    it "should have a name" do
+      @product.name = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "should not be valid without a description" do
+      @product.description = nil
+      expect(@product).to be_valid
+    end
+
+    it "should not be valid without a price" do
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "should not be valid without a quantity" do
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "should not be valid without a category" do
+      @product.category = nil
+      expect(@product).to_not be_valid
+    end
+  end
 end
